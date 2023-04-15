ea40215fb7ac859b3f69e64a469b12ddb2bc2abe
gojaeng <gojh1013@gmail.com>
2023-04-15 18:13:08 +0900

    state 문법

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  
-  let post = '구로 우동 맛집';
-  
+  let [글제목, 글제목변경] = useState(["남자 코트 추천", "구로 맛집", "React 공부법"]);
+  let [따봉,따봉변경] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{post}</h4>
+      <button onClick={()=> { 
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+        }}>글수정</button>
+      <div className="list">
+        <h4>
+          {글제목[0]} 
+          <span onClick={()=> { 따봉변경(따봉+1) }}>👍</span> {따봉}
+        </h4>
+        <p>4월 15일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>4월 15일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>4월 15일 발행</p>
+      </div>
     </div>
   );
 }
