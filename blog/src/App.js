e7d5b02e023e798f162d03e549b3cecf36b5adce
gojaeng <gojh1013@gmail.com>
2023-04-15 16:40:45 +0900

    프로젝트 생성

---
+++ blog/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  let post = '구로 우동 맛집';
+  
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>블로그</h4>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
+}
+
+export default App;
