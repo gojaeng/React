8634446e98af62660c2c0d431e8ce9ec1e9b57fa
gojaeng <gojh1013@gmail.com>
2023-04-16 18:34:22 +0900

    component 사용법

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,23 +5,38 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let [글제목, 글제목변경] = useState(["남자 코트 추천", "구로 맛집", "React 공부법"]);
-  let [따봉,따봉변경] = useState(0);
-
+  let [글제목, 글제목변경] = useState([
+    "남자 코트 추천",
+    "구로 맛집",
+    "React 공부법",
+  ]);
+  let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <button onClick={()=> { 
-        let copy = [...글제목];
-        copy[0] = '여자 코트 추천';
-        글제목변경(copy);
-        }}>글수정</button>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+        }}
+      >
+        글수정
+      </button>
       <div className="list">
         <h4>
-          {글제목[0]} 
-          <span onClick={()=> { 따봉변경(따봉+1) }}>👍</span> {따봉}
+          {글제목[0]}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {따봉}
         </h4>
         <p>4월 15일 발행</p>
       </div>
@@ -32,11 +47,24 @@ function App() {
       </div>
 
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={()=>{setModal(!modal)}}>{글제목[2]}</h4>
         <p>4월 15일 발행</p>
       </div>
+      
+      {
+        modal == true? <Modal></Modal> : null
+      }
+
     </div>
   );
 }
-
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+}
 export default App;
