2dafec98158eea62c1bfb60304dd33751a0e96e8
gojaeng <gojh1013@gmail.com>
2023-05-24 19:56:21 +0900

    styled-components

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,11 +1,26 @@
+import React from "react";
 import { useParams } from "react-router-dom";
+import {useEffect} from "react";
+import styled from 'styled-components'
 
+let YellowBtn= styled.button`
+  background: yellow;
+  color: black;
+  padding: 10px
+`
+let Box = styled.div`
+  background :grey;
+  padding: 20px;
+`
 function Detail(props) {
-  
-  let {id} = useParams();
 
-  return (
+  let {id} = useParams();
+  let 찾은상품 = props.shoes.find(x=> x.id==id);
+
+  return ( 
     <div className="container">
+      <YellowBtn>버튼</YellowBtn>
+      <Box>박스</Box>
       <div className="row">
         <div className="col-md-6">
           <img
