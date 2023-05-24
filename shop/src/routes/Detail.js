32f62bdd0fbfe5f79fa4f9f72007ccee76069305
gojaeng <gojh1013@gmail.com>
2023-05-24 20:43:29 +0900

    useEffect

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,26 +1,40 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import {useEffect} from "react";
-import styled from 'styled-components'
+import { useState } from "react";
+import { cleanup } from "@testing-library/react";
 
-let YellowBtn= styled.button`
-  background: yellow;
-  color: black;
-  padding: 10px
-`
-let Box = styled.div`
-  background :grey;
-  padding: 20px;
-`
 function Detail(props) {
 
   let {id} = useParams();
   let 찾은상품 = props.shoes.find(x=> x.id==id);
+  let [count, setCount] = useState(0)
+  let [alert, setalert] = useState(true);
+  let [num, setNum] = useState('');
 
+  useEffect(()=>{
+    let a =setTimeout(()=>{setalert(false)},2000)
+    return ()=>{
+      clearTimeout(a)
+    }
+  }, [count])
+
+  useEffect(()=>{
+    if(isNaN(num)==true){
+      alert('숫자만 입력하세요.')
+    }
+  },[num])
   return ( 
     <div className="container">
-      <YellowBtn>버튼</YellowBtn>
-      <Box>박스</Box>
+      {
+        alert==true
+        ? <div className="alert alert-warning">
+          2초 이내 구매시 할인
+          </div>
+        :null
+      }
+      {count}
+      <button onClick={()=>{setCount(count+1)}}>버튼</button>
       <div className="row">
         <div className="col-md-6">
           <img
@@ -32,6 +46,7 @@ function Detail(props) {
           <h4 className="pt-5">{props.shoes[id-1].title}</h4>
           <p>{props.shoes[id-1].content}</p>
           <p>{props.shoes[id-1].price}</p>
+          <input onChange={(e)=>{setNum(e.target.value)}}/>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
