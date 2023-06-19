444fd738a1379dd044f5f52bd1505991f3046746
고재현 <gojh1013@gmail.com>
2023-06-19 16:05:25 +0900

    async 관련

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [age, setAge] = useState(20);
+
+  useEffect(()=>{
+    if(count<3 && count!=0){
+      setAge(age+1)
+    }
+  },[count])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>안녕하십니까 전 {age}</div>
+      <button onClick={()=>{
+        setCount(count+1);
+      }}>
+        누르면 한살 먹기</button>
     </div>
   );
 }
