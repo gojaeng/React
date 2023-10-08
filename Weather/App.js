593939a962cdac734d96948913fd1ebb5762c955
unknown <gojh1013@gmail.com>
2023-10-08 21:13:33 +0900

    Feat : Weather app part1

--- Weather/App.js
+++ Weather/App.js
@@ -3,10 +3,49 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
     return (
-        <View style={{ flex: 1 }}>
-            <View style={{ flex: 1, backgroundColor: 'tomato' }}></View>
-            <View style={{ flex: 1, backgroundColor: 'teal' }}></View>
-            <View style={{ flex: 1, backgroundColor: 'orange' }}></View>
+        <View style={styles.container}>
+            <View style={styles.city}>
+                <Text style={styles.cityName}>Seoul</Text>
+            </View>
+            <View style={styles.weather}>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#a0b5e9',
+    },
+    city: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    cityName: {
+        color: 'black',
+        fontSize: 58,
+        fontWeight: '500',
+    },
+    weather: {
+        flex: 3,
+    },
+    day: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    temp: {
+        fontSize: 178,
+        marginTop: 50,
+    },
+    description: {
+        marginTop: -30,
+        fontSize: 60,
+    },
+});
