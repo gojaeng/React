42f36e134243bc7c84c4d1564d7eec8018c902c0
unknown <gojh1013@gmail.com>
2023-10-09 01:16:03 +0900

    Feat: Weather app part3

--- Weather/App.js
+++ Weather/App.js
@@ -1,17 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, ScrollView, Dimensions, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
 export default function App() {
+    const [city, setCity] = useState('Loading...');
+    const [location, setLocation] = useState();
+    const [ok, setOk] = useState(true);
+    const ask = async () => {
+        const { granted } = await Location.requestForegroundPermissionsAsync();
+        if (!granted) {
+            setOk(false);
+        }
+        const {
+            coords: { latitude, longitude },
+        } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+        const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
+        setCity(location[0].city);
+    };
+    useEffect(() => {
+        ask();
+    }, []);
+
     return (
         <View style={styles.container}>
             <View style={styles.city}>
-                <Text style={styles.cityName}>Seoul</Text>
+                <Text style={styles.cityName}>{city}</Text>
             </View>
             <ScrollView
                 horizontal
                 pagingEnabled
-                showsHorizontalScrollIndicator="false"
+                showsHorizontalScrollIndicator={false}
                 contentContainerStyle={styles.weather}
             >
                 <View style={styles.day}>
