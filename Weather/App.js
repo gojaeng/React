5236498742164635ed216abe3ece9377771b749b
unknown <gojh1013@gmail.com>
2023-10-13 21:24:24 +0900

    Feat : Add weather  icons

--- Weather/App.js
+++ Weather/App.js
@@ -2,16 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, ScrollView, Dimensions, StyleSheet } from 'react-native';
 import * as Location from 'expo-location';
 import { ActivityIndicator } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
+
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
     const [city, setCity] = useState('Loading...');
     const [ok, setOk] = useState(true);
     const [days, setDays] = useState([]);
+    const icons = {
+        Clouds: 'cloudy',
+        Clear: 'day-sunny',
+        Atmosphere: 'cloudy-gusts',
+        Snow: 'snow',
+        Rain: 'rains',
+        Drizzle: 'rain',
+        Thunderstorm: 'lightning',
+    };
     const API_KEY = 'bb542442b43673cb197792c8ac2d4994';
     const getLocalDateTime = (timestamp) => {
         const date = new Date(timestamp * 1000);
-        const localDate = new Date(date.getTime() + date.getTimezoneOffset() * 60000); // 로컬 타임으로 변환
+        const localDate = new Date(date.getTime() + date.getTimezoneOffset() * 60000);
         const options = {
             year: 'numeric',
             month: 'long',
@@ -19,6 +30,7 @@ export default function App() {
         };
         return localDate.toLocaleString('ko-KR', options);
     };
+
     const getWeather = async () => {
         const { granted } = await Location.requestForegroundPermissionsAsync();
         if (!granted) {
@@ -35,11 +47,12 @@ export default function App() {
         const json = await response.json();
         console.log(json);
         setDays(
-            json.list.filter((weather) => {
-                if (weather.dt_txt.includes('00:00:00')) {
-                    return weather;
-                }
-            })
+            json.list
+                .filter((weather) => weather.dt_txt.includes('03:00:00'))
+                .map((weather) => ({
+                    ...weather,
+                    dt_txt: getLocalDateTime(weather.dt),
+                }))
         );
     };
 
@@ -59,14 +72,25 @@ export default function App() {
                 contentContainerStyle={styles.weather}
             >
                 {days.length === 0 ? (
-                    <View style={styles.day}>
+                    <View style={{ ...styles.day, alignItems: 'center' }}>
                         <ActivityIndicator color="white" size="large" style={{ marginTop: 10 }} />
                     </View>
                 ) : (
                     days.map((day, index) => (
                         <View key={index} style={styles.day}>
-                            <Text style={styles.date}>{getLocalDateTime(day.dt - 1)}</Text>
-                            <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+                            <View
+                                style={{
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    width: '100%',
+                                    justifyContent: 'space-between',
+                                    marginBottom: 5,
+                                }}
+                            >
+                                <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+                                <Fontisto name={icons[day.weather[0].main]} size={68} color="black" />
+                            </View>
+                            <Text style={styles.date}>{getLocalDateTime(day.dt)}</Text>
                             <Text style={styles.description}>{day.weather[0].main}</Text>
                             <Text style={styles.tinyText}>{day.weather[0].description}</Text>
                         </View>
@@ -80,35 +104,45 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#a0b5e9',
+        backgroundColor: 'lightgrey',
     },
     city: {
-        flex: 1,
+        flex: 1.2,
         justifyContent: 'center',
         alignItems: 'center',
     },
     cityName: {
-        color: 'black',
         fontSize: 58,
         fontWeight: '500',
+        color: 'black',
     },
     weather: {},
     day: {
-        alignItems: 'center',
         width: SCREEN_WIDTH,
-        flex: 1,
+        alignItems: 'flex-start',
+        paddingHorizontal: 20,
     },
     temp: {
-        fontSize: 178,
         marginTop: 50,
+        fontWeight: '600',
+        fontSize: 100,
+        color: 'black',
     },
     description: {
-        marginTop: -30,
-        fontSize: 60,
+        marginTop: -10,
+        fontSize: 30,
+        color: 'black',
+        fontWeight: '500',
+    },
+    tinyText: {
+        marginTop: -5,
+        fontSize: 25,
+        color: 'black',
+        fontWeight: '500',
     },
     date: {
         fontSize: 32,
-        marginBottom: 10,
+        marginTop: 10,
         color: 'black',
     },
 });
