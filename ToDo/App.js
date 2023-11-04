bd181c36c9821c95fee600d08472809781b41a7b
unknown <gojh1013@gmail.com>
2023-11-04 21:53:47 +0900

    Feat : TouchableOpacity & TouchableHighlight

--- ToDo/App.js
+++ ToDo/App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { theme } from './color';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <View style={styles.header}>
+                <TouchableOpacity>
+                    <Text style={styles.btnText}>Work</Text>
+                </TouchableOpacity>
+                <TouchableHighlight backgroundColor="red">
+                    <Text style={styles.btnText}>Travel</Text>
+                </TouchableHighlight>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: theme.bg,
+        paddingHorizontal: 20,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 100,
+    },
+    btnText: {
+        color: 'white',
+        fontSize: 38,
+        fontWeight: 600,
+    },
 });
